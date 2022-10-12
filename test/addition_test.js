4b73a0de66e1c5b5094f1da2884d749244f91889
drl <darululm4@gmail.com>
2022-10-12 16:57:18 +0700

    Create test for addition function

---
+++ test/addition_test.js
@@ -0,0 +1,21 @@
+let { addition } = require('../lib/Calculator')
+let assert = require('chai').assert
+
+describe('Test addition function', function(){
+
+	it('Should be 2', function(){
+		assert.equal(addition(1,1), 2)
+	})
+
+	it('Should be 10', function(){
+		assert.equal(addition(5,5), 10)
+	})
+
+	it('Should be 100', function(){
+		assert.equal(addition(10,90), 100)
+	})
+
+	it('Should be 1000', function(){
+		assert.equal(addition(1,999), 1000)
+	})
+})
